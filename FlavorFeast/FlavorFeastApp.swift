2edf167d33918915a3fac81c004a1f568a4d39f0
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2024-05-28 13:02:06 +0200

    Initial Commit

---
+++ FlavorFeast/FlavorFeastApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorFeastApp.swift
+//  FlavorFeast
+//
+//  Created by Marc Santisteban Ruiz on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FlavorFeastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
