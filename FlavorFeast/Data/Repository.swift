aace98cf19cabe1c52e402c74abb3d58a9826150
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2024-06-05 13:25:26 +0200

    Add all to repository

--- FlavorFeast/Data/Repository.swift
+++ FlavorFeast/Data/Repository.swift
@@ -19,4 +19,76 @@ final class Repository: RepositoryInterface {
             }
             .eraseToAnyPublisher()
     }
+    
+    func getAllCategories() -> AnyPublisher<[CategoryUI], Error> {
+        return networkDataSource
+            .getAllCategories()
+            .tryMap { response in
+                return response.map { $0.toUI() }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getAllAreas() -> AnyPublisher<[SingleAreaUI], Error> {
+        return networkDataSource
+            .getAllAreas()
+            .tryMap { response in
+                return response.map { $0.toUI() }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getAllIngredients() -> AnyPublisher<[IngredientsUI], Error> {
+        return networkDataSource
+            .getAllIngredients()
+            .tryMap { response in
+                return response.map { $0.toUI() }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getByCategory(category: String) -> AnyPublisher<[SingleMealUI], Error> {
+        return networkDataSource
+            .getByCategory(category: category)
+            .tryMap { response in
+                return response.map { $0.toUI() }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getByArea(area: String) -> AnyPublisher<[SingleMealUI], Error> {
+        return networkDataSource
+            .getByArea(area: area)
+            .tryMap { response in
+                return response.map { $0.toUI() }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getByIngredient(ingredient: String) -> AnyPublisher<[SingleMealUI], Error> {
+        return networkDataSource
+            .getByIngredient(ingredient: ingredient)
+            .tryMap { response in
+                return response.map { $0.toUI() }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getByID(id: String) -> AnyPublisher<MealUI, Error> {
+        return networkDataSource
+            .getByID(id: id)
+            .tryMap { response in
+                return response.toUI()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getByName(searchText: String) -> AnyPublisher<[MealUI], Error> {
+        return networkDataSource
+            .getByName(searchText: searchText)
+            .tryMap { response in
+                return response.map { $0.toUI() }
+            }
+            .eraseToAnyPublisher()
+    }
 }
--- FlavorFeast/Data/RepositoryInterface.swift
+++ FlavorFeast/Data/RepositoryInterface.swift
@@ -3,4 +3,12 @@ import Combine
 
 protocol RepositoryInterface {
     func getRandomMeal() -> AnyPublisher<MealUI, Error>
+    func getAllCategories() -> AnyPublisher<[CategoryUI], Error>
+    func getAllAreas() -> AnyPublisher<[SingleAreaUI], Error>
+    func getAllIngredients() -> AnyPublisher<[IngredientsUI], Error>
+    func getByCategory(category: String) -> AnyPublisher<[SingleMealUI], Error>
+    func getByArea(area: String) -> AnyPublisher<[SingleMealUI], Error>
+    func getByIngredient(ingredient: String) -> AnyPublisher<[SingleMealUI], Error>
+    func getByID(id: String) -> AnyPublisher<MealUI, Error>
+    func getByName(searchText: String) -> AnyPublisher<[MealUI], Error>
 }
